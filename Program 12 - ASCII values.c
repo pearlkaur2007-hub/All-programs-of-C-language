c3a9f9d226a31ea8804742645bbfd20aceff7a37
Pearl Kaur <pearlkaur2007@gmail.com>
2025-08-30 09:37:45 +0530

    Add files via upload

---
+++ Program 12 - ASCII values.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+WAPC to input a character. Print its ASCII value.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char ipChar;
+    printf("\nEnter a character constant: ");
+    scanf("%c",&ipChar);
+    printf("\nThe ASCII value of %c is %d",ipChar,ipChar);
+    return 0;
+}
