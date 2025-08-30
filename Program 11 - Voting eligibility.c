2794280910bc11a411d698cb6f2e2431bcc83389
Pearl Kaur <pearlkaur2007@gmail.com>
2025-08-30 09:37:21 +0530

    Add files via upload

---
+++ Program 11 - Voting eligibility.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+WAPC to input the age of a person. Check and display if the person is eligible to vote.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    unsigned int age; 
+    printf("\nEnter your age: ");
+    scanf( "%u", &age);
+    if(age<18)
+    {
+        printf("\nYou are not eligible to vote. ");
+    }
+    else
+    {
+        printf("\nYou are eligible to vote. ");
+    }
+    
+    return 0;
+}
