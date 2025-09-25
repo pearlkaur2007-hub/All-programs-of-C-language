9948a2f0ec0ad48edf55ae81f240e1809652b8aa
Pearl Kaur <pearlkaur2007@gmail.com>
2025-09-25 10:16:19 +0530

    Add files via upload

---
+++ Program 31 - sum of first n odd and even numbers separately.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+WAPC to display the sum of the first ‘n’ odd numbers and the sum of the first 
+‘n’ even numbers separately. Here, ‘n’ is the user input.
+*******************************************************************************/
+#include<stdio.h>
+
+int main()
+{
+    unsigned int num; int i, sum=0, sum1=0;
+    printf("\nEnter a number: ");
+    scanf("%d", &num);
+    for(i=1;i<=num;i+=2)
+    {
+        sum+=i;
+    }
+    printf("\n Sum of odd numbers = %d", sum);
+    for(i=2;i<=num;i+=2)
+    {
+        sum1+=i;
+    }
+    printf("\n Sum of even numbers = %d", sum1);
+    return 0;
+}
