8fe6d7471ac55c71921a9082cf7e203a490fdb1a
Pearl Kaur <pearlkaur2007@gmail.com>
2025-09-28 17:49:54 +0530

    Add files via upload

---
+++ Program 35 - Perfect Number Check.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+WAPC to input a positive integer. Check and print if the number is perfect or 
+not. Note: Perfect number is a number which is equal to the sum of all its proper 
+divisors (excluding itself). Example: 28 = 1 + 2 + 4 + 7 + 14
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    unsigned int num; int i, sum=0;
+    printf("\nEnter a number: ");
+    scanf("%u", &num);
+    for(i=1;i<num;++i)
+    {
+        if(num%i==0)
+        {
+            sum+=i;
+        }
+    }
+    printf("\n Sum = %d", sum);
+    if(sum==num)
+    {
+        printf("\n%d is a perfect number.", num);
+    }
+    else
+    {
+        printf("\n%d is not a perfect number.", num);
+    }
+    return 0;
+}
